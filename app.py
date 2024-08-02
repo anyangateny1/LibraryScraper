c665fdc965d26a2b178a5cb845b2bdbf27fff946
anyangateny1 <a1853103@adelaide.edu.au>
2024-08-02 18:39:41 +0930

    Final changes for MVP

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request
-
 from scraper import scrapeLibrary
 
 app = Flask(__name__)
@@ -11,13 +10,9 @@ def index():
 @app.route('/search', methods=['POST'])
 def search():
     if request.method == 'POST':
-
         book_name = request.form['query']
-
         result = scrapeLibrary(book_name)
-
         return render_template('result.html', result=result)
     
 if __name__ == '__main__':
-    
     app.run(debug=True)
--- scraper.py
+++ scraper.py
@@ -1,94 +1,51 @@
-def scrapeLibrary(bookName): 
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
-    # imports webdriver
-    from selenium import webdriver
-
-    # import Action Chains
-    from selenium.webdriver.common.action_chains import ActionChains
-
-    #import keys
-    from selenium.webdriver.common.keys import Keys
-    
-    #import by
-    from selenium.webdriver.common. by import By
-
-    # creates webdriver Object
-
+def scrapeLibrary(bookName):
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
     options.add_argument("window-size=1920,1080")
+    options.add_experimental_option(
+
+        "prefs", {"profile.managed_default_content_settings.images": 2}
+    )
 
     driver = webdriver.Chrome(options=options)
-
-    #  recieves website
-
     driver.get("https://onecard.network/client/en_AU/playford/")
-
-    # waits for page to load
-    
     driver.implicitly_wait(5)
 
-    # inputs bookName into the search box
-
-    ActionChains(driver)\
-        .send_keys(bookName, Keys.ENTER)\
-        .perform()
-
+    ActionChains(driver).send_keys(bookName, Keys.ENTER).perform()
     driver.implicitly_wait(5)
 
     restrictToBooks = driver.find_elements(By.LINK_TEXT, "Books")
     
     if restrictToBooks:
-
         restrictToBooks = driver.find_element(By.LINK_TEXT, "Books").click()
-    
     else:
-
-        print(bookName, "does not exist as a book.")
-
-        return
-    
-    # gets the location of the title of the book and attempts to match to the first pop up
+        driver.close()
+        return {"error": f"{bookName} does not exist as a book."}
 
     column1 = driver.find_element(By.ID, "results_cell0")
-
-    bookTitle =  column1.find_element(By.CLASS_NAME, "displayDetailLink").text
-
-    # if it matches the first pop up, proceed with interacting with the book otherwise exit driver
+    bookTitle = column1.find_element(By.CLASS_NAME, "displayDetailLink").text
 
     if bookName.lower() in bookTitle.lower():
-    
         bookPicture = driver.find_elements(By.CLASS_NAME, "stupid_ie_div")
-    
         if bookPicture:
-
             driver.find_element(By.CLASS_NAME, "stupid_ie_div").click()
-        
         else:
-        
-            print ("Book title does not match any books in SA Libraries.")
-
-            return    
-
-    # else function is if selenium is unable to find bookPicture element means that there are no search results avaiable at all.
-    #  
+            driver.close()
+            return {"error": "Book title does not match any books in SA Libraries."}
     else:
-        
-        print ("Book title does not match any books in SA Libraries.")
-
-        return
+        driver.close()
+        return {"error": "Book title does not match any books in SA Libraries."}
 
     driver.implicitly_wait(100)
-    # finds the area of the status and location of the book
     area = driver.find_element(By.ID, "tabs-1")
-
     location = area.find_element(By.CLASS_NAME, "asyncFieldLIBRARY").text
-
-     # returns the text of the status and location
+    status = area.find_element(By.CLASS_NAME, "asyncFieldSD_ITEM_STATUS").text
 
     driver.close()
     
-    return bookTitle, location
-
- 
-    
+    return {"title": bookTitle, "location": location, "status": status}
